bf7806c06840048fbc33bccd3842ec69b1732e04
Bergony <Bergony@hotmail.com>
2021-05-09 21:01:07 -0300

    Reaction Timer Initial Project

---
+++ PROJECT/reaction-timer/src/components/Block.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="block" v-if="showBlock">
+      Click Me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data () {
+        return{
+            showBlock: false
+        }
+    },
+    mounted() {
+        console.log('componet mounted'),
+        setTimeout(() => {
+            this.showBlock = true
+            console.log(this.delay)
+        }, this.delay)
+    },
+    updated () {
+        console.log('Component update')
+
+    },
+    unmounted() {
+        console.log('Unmounted')
+    }
+}
+</script>
+
+<style>
+
+.block {
+    width: 400px;
+    border-radius: 20px;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+
+</style>
