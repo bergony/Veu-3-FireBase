ffab7776eb515d009ee56c615af6e5a638c1ce8a
Bergony <Bergony@hotmail.com>
2021-05-23 16:25:18 -0300

    Router jobs

---
+++ ninja-jobs/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>HomePage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima a minus iusto porro cum eum, soluta corporis sequi hic, fuga excepturi quis possimus, veritatis quam officiis sed. Sunt exercitationem minima laudantium architecto eveniet iusto perferendis. Ab consequatur impedit molestiae, minus delectus ipsa in obcaecati adipisci veniam possimus optio voluptatem, debitis labore excepturi natus molestias maiores fugit numquam illo laudantium quaerat corporis nobis voluptates? Vitae rem, minima officia in aliquam sunt magni eligendi consequatur eum, iste accusantium iure reprehenderit exercitationem ducimus dignissimos asperiores quaerat numquam nesciunt ipsa? Amet rem autem perspiciatis magnam sapiente doloribus. Velit soluta perspiciatis esse blanditiis. Fugit, nesciunt.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima a minus iusto porro cum eum, soluta corporis sequi hic, fuga excepturi quis possimus, veritatis quam officiis sed. Sunt exercitationem minima laudantium architecto eveniet iusto perferendis. Ab consequatur impedit molestiae, minus delectus ipsa in obcaecati adipisci veniam possimus optio voluptatem, debitis labore excepturi natus molestias maiores fugit numquam illo laudantium quaerat corporis nobis voluptates? Vitae rem, minima officia in aliquam sunt magni eligendi consequatur eum, iste accusantium iure reprehenderit exercitationem ducimus dignissimos asperiores quaerat numquam nesciunt ipsa? Amet rem autem perspiciatis magnam sapiente doloribus. Velit soluta perspiciatis esse blanditiis. Fugit, nesciunt.</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
