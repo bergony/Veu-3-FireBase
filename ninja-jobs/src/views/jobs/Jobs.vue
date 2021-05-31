e98560b3ca7f98b808e83dc5ef68a5ebdff28ac9
Bergony <Bergony@hotmail.com>
2021-05-31 14:56:39 -0300

    Finish Jobs

--- ninja-jobs/src/views/jobs/Jobs.vue
+++ ninja-jobs/src/views/jobs/Jobs.vue
@@ -1,9 +1,15 @@
 <template>
+
     <h1>jobs</h1>
-    <div v-for="job in jobs" :key="job.id" class="job">
-       <router-link :to="{ name: 'JobDetails', params: { id: job.id}}">
-            <h2>{{ job.title }}</h2>
-       </router-link>
+    <div v-if="jobs.length">
+        <div v-for="job in jobs" :key="job.id" class="job">
+        <router-link :to="{ name: 'JobDetails', params: { id: job.id}}">
+                <h2>{{ job.title }}</h2>
+        </router-link>
+        </div>
+    </div>
+    <div v-else>
+        <p>Loading Jobs ...</p>
     </div>
   
 </template>
@@ -13,11 +19,15 @@ export default {
     data(){
         return {
             jobs: [
-                {title: 'Ninja UX Designer', id:1, details: 'Lorm '},
-                {title: 'Ninja WEB Designer', id:2, details: 'Lorm '},
-                {title: 'Ninja Vue Designer', id:3, details: 'Lorm '},
+
             ]
         }
+    },
+    mounted () {
+        fetch('http://localhost:3000/jobs')
+        .then(res => res.json())
+        .then(data => this.jobs = data)
+        .catch(err => console.log(err.message))
     }
 
 }
